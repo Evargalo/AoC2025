a2f8e3bb1d4ff2fbaaff68a306f37c84001fd3d8
Olivier Pucher <olivier.pucher@insee.fr>
2025-12-01 09:38:23 +0100

    Update AoC_25_D1.py

--- AoC_25_D1.py
+++ AoC_25_D1.py
@@ -4,8 +4,8 @@ d1 = open('D1.txt', 'r')
 
 pos = 50
 # part 1
-res = 0
-# part 1
+res1 = 0
+# part 2
 res2 = 0
 
 for ligne in d1:
@@ -30,9 +30,9 @@ for ligne in d1:
       pos -= val 
       pos = pos % 100
    if(pos == 0):
-       res += 1 
+       res1 += 1 
 
-print(res)
+print(res1)
 # 1168
 print(res2)
 # 7199
