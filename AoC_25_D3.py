bcbb0c0ecc1202f53f0a07f871a3d10e3fc0a573
Olivier Pucher <olivier.pucher@insee.fr>
2025-12-03 10:36:05 +0100

    Update AoC_25_D3.py

--- AoC_25_D3.py
+++ AoC_25_D3.py
@@ -6,7 +6,7 @@ def pick_batteries(nb_digits) -> int:
     d3 = open('D3.txt', 'r')
     res = 0
     for ligne in d3:
-        l1 = ligne[0:(len(ligne)-1)]
+        l1 = ligne[:-1] # remove last character '\n'
         for k in range(nb_digits-1,-1,-1):
             d = max(l1[0:(len(l1)-k)])
             l1 = l1[(l1.index(d)+1):]
@@ -15,7 +15,7 @@ def pick_batteries(nb_digits) -> int:
 
 # Part 1
 print(pick_batteries(2))
-#16946
+# 16946
 
 # Part 2
 print(pick_batteries(12))
