eeda4e7eac4e02cd3748c7eacc4b62ad1dfaa43a
Olivier Pucher <olivier.pucher@insee.fr>
2025-12-03 09:57:52 +0100

    D3 with function

--- AoC_25_D3.py
+++ AoC_25_D3.py
@@ -1,35 +1,24 @@
 import os
 import builtins
 os.chdir('C:/Users/MMAJR1/Documents/Perso/R for fun/AdventOfCode/AoC2025/')
-d3 = open('D3.txt', 'r')
-# d3 = open('D3test.txt', 'r')
+
+def pick_batteries(nb_digits) -> int: 
+    d3 = open('D3.txt', 'r')
+    res = 0
+    for ligne in d3:
+        l1 = ligne[0:(len(ligne)-1)]
+        for k in range(nb_digits-1,-1,-1):
+            l2 = l1[0:(len(l1)-k)]
+            d = max(l2)
+            rk = l2.index(d)
+            l1 = l1[(rk+1):]
+            res += pow(10,k)*int(d)
+    return res
 
 # Part 1
-res1 = 0
-
-for ligne in d3:
-   l2 = ligne[0:(len(ligne)-2)]
-   d = max(l2)
-   rk = l2.index(d)
-   u = max(ligne[(rk+1):])
-   res1 += 10*int(d)+int(u)
-
-print(res1)
-# 16946
+print(pick_batteries(2))
+#16946
 
 # Part 2
-
-d3 = open('D3.txt', 'r')
-res2 = 0
-
-for ligne in d3:
-   l1 = ligne[0:(len(ligne)-1)]
-   for k in range(11,-1,-1):
-    l2 = l1[0:(len(l1)-k)]
-    d = max(l2)
-    rk = l2.index(d)
-    l1 = l1[(rk+1):]
-    res2 += pow(10,k)*int(d)
-
-print(res2)
+print(pick_batteries(12))
 # 168627047606506
